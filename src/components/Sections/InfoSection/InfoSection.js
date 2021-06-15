4d8ef0270f2305c300e46e150ba2aa04d10a3f58
src-dbgr <src.dbgr@gmail.com>
2021-06-15 21:38:23 +0200

    restructuging

---
+++ src/components/Sections/InfoSection/InfoSection.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function InfoSection() {
+    return (
+        <div>
+            <InfoContainer id={}>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column1>
+                            <TextWrapper>
+                                <TopLine>TopLine</TopLine>
+                                <Heading>Heading</Heading>
+                                <Subtitle>Subtitle</Subtitle>
+                                <BtnWrap>
+                                    <Button to="home">Button</Button>
+                                </BtnWrap>
+                            </TextWrapper>
+                        </Column1>
+                    </InfoRow>
+                </InfoWrapper>
+
+            </InfoContainer>
+        </div>
+    )
+}
+
+export default InfoSection
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,14 @@
 // import React, { useState, useEffect } from "react";
 
-import React, { useState } from "react";
+import Navigation from "../components/Navigation/Navigation";
 import HeroSection from "../components/Sections/HeroSection/HeroSection";
-import Navbar from "../components/Navigation/Navbar/Navbar";
-import Sidebar from "../components/Navigation/Sidebar/Sidebar";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function toggle() {
-    setIsOpen((isOpen) => !isOpen);
-  }
-
-  //   useEffect(() => {
-  //     console.log(isOpen);
-  //   }, [isOpen]);
-
   return (
-    <div>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
+    <>
+      <Navigation />
       <HeroSection />
-    </div>
+    </>
   );
 };
 
