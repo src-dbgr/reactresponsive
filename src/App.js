7aa385e83c16af0b8966f14fc000cf82d8e3dde9
src-dbgr <src.dbgr@gmail.com>
2021-06-15 15:15:00 +0200

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <div className="App">Dev</div>;
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
 }
 
 export default App;
