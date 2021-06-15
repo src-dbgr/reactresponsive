2b837631926eff6c5e4065a2abefdea8a70e1b4a
src-dbgr <src.dbgr@gmail.com>
2021-06-16 01:51:29 +0200

    added social icons, stransparent hnavbar and scroll to top on click

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { animateScroll as scroll } from "react-scroll";
 import {
   MobileIcon,
   Nav,
@@ -13,32 +15,56 @@ import {
 } from "./NavbarElements";
 
 const Navbar = (props) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  function changeNav() {
+    if (window.scrollY >= 80) {
+      setScrollNav((scrollNav) => true);
+    } else {
+      setScrollNav((scrollNav) => false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, [scrollNav]);
+
+  function toggleHome() {
+    scroll.scrollToTop();
+  }
+
   return (
-    <Nav>
-      <NavbarContainer>
-        <NavLogo to="/">dolla</NavLogo>
-        <MobileIcon onClick={props.toggle}>
-          <FaBars />
-        </MobileIcon>
-        <NavMenu>
-          <NavItem>
-            <NavLinks to="about">About</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks to="discover">Discover</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks to="services">Services</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks to="/signin">Sign Up</NavLinks>
-          </NavItem>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to="/signin">Sign In</NavBtnLink>
-        </NavBtn>
-      </NavbarContainer>
-    </Nav>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/" onClick={toggleHome}>
+              dolla
+            </NavLogo>
+            <MobileIcon onClick={props.toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="about">About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="discover">Discover</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/signin">Sign Up</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
   );
 };
 
--- src/components/Navigation/Navbar/NavbarElements.js
+++ src/components/Navigation/Navbar/NavbarElements.js
@@ -3,9 +3,9 @@ import { Link as LinkScroll } from "react-scroll";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #000;
+  background: ${({ scrollNav }) => (scrollNav ? "#000" : "transparent")};
   height: 80px;
-  /* margin-top: -80px; */
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
